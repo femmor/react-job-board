0cbc4f298e24be4ee9d86201bbb22c23b450a764
Emmanuel Egomson <fegomson@gmail.com>
2020-12-16 17:56:18 -0600

    project complete

--- src/App.js
+++ src/App.js
@@ -4,20 +4,82 @@ import React, {useState, useEffect} from 'react'
 import JobBoardComponent from "./components/JobBoardComponent"
 import data from "./assets/data.json"
 
+import bgImage from "./assets/images/bg-header-desktop.svg"
+
 const App = () => {
   
   const [jobs, setJobs] = useState([])
+  const [filters, setFilters] = useState([])
 
   useEffect(() => setJobs(data), [])
-  
+
+  const filterFunc = ({role, level, tools, languages}) => {
+
+    const tags = [role, level];
+
+    if (filters.length === 0) {
+      return true
+    }
+
+    if (tools) {
+        tags.push(...tools)
+    }
+
+    if (languages) {
+        tags.push(...languages)
+    }
+
+    return tags.some(tag => filters.includes(tag))
+  }
+
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) return;
+
+    setFilters([...filters, tag])
+  }
+
+  const handleFilterRemove = (passedFilter) => {
+    setFilters(filters.filter((f) => f !== passedFilter))
+  }
+
+  const filteredJobs = jobs.filter(filterFunc)
+
+  const clearFilters = () => {
+    setFilters([])
+  }
+
   return (
-    <>
-      {
-        jobs.length === 0 ? (<p>Fetching jobs...</p>) : (
-          jobs.map(job => <JobBoardComponent job={job} key={job.id}/>)
-        )
-      }
-    </>
+    <div className="bg-green-100">
+      <header className="bg-green-400 py-16 " style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundPosition: 'center',
+        backgroundSize: "cover",
+      }}>
+      </header>
+      <div className="py-5 container m-auto">
+        <div>
+          {
+            filters.length > 0 && <div className="flex flex-wrap justify-between items-center space-x-4 bg-white shadow-md m-4 p-6 mt-16 md:mt-0 rounded border-l-4">
+            <div>
+            <span className="text-xl py-1">Filters: </span>
+            <span>
+            {filters.length > 0 && filters.map(filter => 
+              (<span onClick={() => handleFilterRemove(filter)} className="cursor-pointer px-2 pb-1 mr-2 text-green-600 font-semibold tracking-tight text-md bg-green-100 mb-2 relative">x {filter}</span>))}
+            </span>
+            </div>
+            <span>
+              {filters.length > 0 && <button className="bg-red-600 text-white px-3 py-1 focus:outline-none" onClick={() => clearFilters()}>Clear</button>}
+            </span>
+          </div>
+          }
+          {
+            jobs.length === 0 ? (<p>Fetching jobs...</p>) : (
+              filteredJobs.map(job => <JobBoardComponent job={job} key={job.id} handleTagClick={handleTagClick}/>)
+            )
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/JobBoardComponent.jsx
+++ src/components/JobBoardComponent.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 
-const JobBoardComponent = () => {
+const JobBoardComponent = ({ job: { id, newJob, company, logo, featured, position, role, level, postedAt, contract, location, languages, tools }, handleTagClick}) => {
+
+    const tags = [role, level];
+
+    if (tools) {
+        tags.push(...tools)
+    }
+
+    if (languages) {
+        tags.push(...languages)
+    }
+
     return (
-        <div>
-            This is the job board component
+        <div className="mt-8">
+            <div className="flex md:flex-row flex-col bg-white shadow-md m-4 p-6 mt-16 md:mt-0 rounded border-l-4 border-green-600">
+                <div>
+                    <img src={logo} alt="job-logo" className="w-20 h-20 -mt-16 md:mt-0"/>
+                </div>
+                <div className="flex flex-col ml-4 mt-4 md:mt-0 justify-between">
+                    <div className="flex items-center space-x-4">
+                        <h3 className="text-green-500 font-bold">{company}</h3>
+                        {newJob && <span className="uppercase bg-green-400 px-2 py-1 text-white text-xs font-semibold rounded-full">new!</span>}
+                        {featured && <span className="uppercase bg-gray-400 px-2 py-1 text-white text-xs font-semibold rounded-full">featured</span>}
+                    </div>
+                    <h2 className="font-bold text-xl">{position}</h2>
+                    <p className="text-gray-700">
+                        {postedAt} . {contract} . {location}
+                    </p>
+                </div>
+                <div className="border border-gray-100 mt-2 md:hidden">
+
+                </div>
+                <div className="flex flex-wrap items-center md:ml-auto m-4">
+                    {
+                        tags ? tags.map(tag => (
+                            <button onClick={() => handleTagClick(tag)} className="px-2 py-1 mr-2 text-green-600 font-semibold tracking-tight text-md bg-green-100 mb-2">{tag}</button>
+                        )) : null
+                    } 
+                </div>
+            </div>
         </div>
     )
 }
